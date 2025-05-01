7f453140784d284300091f14a99b5bd26c556c4c
Shashank Gupta <shashankg2705@gmail.com>
2025-05-01 23:52:12 +0530

    Fixed connection

--- config/schoolDB.js
+++ config/schoolDB.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const schoolPool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+const connectionURL = "mysql://root:ulZyGUYRhCXvhINcpFRqOMTItjkFbREc@metro.proxy.rlwy.net:59314/railway";
 
-module.exports = schoolPool.promise();
+const connection = mysql.createConnection(connectionURL);
+
+// const schoolPool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+// });
+
+module.exports = connection;
